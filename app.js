b372bc85c0acfbdb1359e40e91563befae2e0898
sm <a0086845@u.nus.edu>
2017-11-29 15:58:18 +0800

    created modal template for feedback form

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const vm = new Vue({
   template: `
     <div>
       <h1>Hi {{ user.name }}</h1>
+      <h2>Thank you for ordering with us. Your feedback is important in helping us imporve and design new exciting meals in the future.</h2>
       <div v-for="order in showAllOrders.orders">
-        {{ order }}
-        <br>
+        <order-item :order=order></order-item>
         <br>
       </div>
     </div>
@@ -28,6 +28,49 @@ const vm = new Vue({
 
     showOrderItem() {
       //
+    }
+  }
+})
+
+Vue.component("order-item", {
+  props: {
+    order: {
+      type: Object
+    },
+    showModal: false
+  },
+  data: function() {
+    return {
+      mutableShowModal: this.showModal
+    }
+  },
+  template: `
+  <div>
+    {{order}}
+    <p>Order ID: {{order.order_id}}</p>
+    <p>Delivery Date: {{order.delivery_date}} at {{order.delivery_time}}</p>
+
+    <button id="show-modal" @click="mutableShowModal = true">Give Feedback!</button>
+    <modal v-if="mutableShowModal" @close="mutableShowModal = false" :order=order></modal>
+  </div>
+  `,
+  methods: {
+    submitFeedback: function() {
+      console.log("submited" + this.order.id)
+    }
+  }
+})
+
+Vue.component("modal", {
+  props: {
+    order: {
+      type: Object
+    }
+  },
+  template: "#modal-template",
+  methods: {
+    submitFeedback: function() {
+      console.log("submited " + this.order.id)
     }
   }
 })
